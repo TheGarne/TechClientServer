b2d7d07eaa3be596aa9bb4d4fa5ea39aff7dd845
Asbjørn Stahl Garne <asbjoern@garne.dk>
2020-05-21 10:28:55 +0200

    Proper validation of messagenumbers, IP-address etc.

--- Tech - Client/Client.py
+++ Tech - Client/Client.py
@@ -14,33 +14,43 @@ def heartbeat(client, lock):
         client.send("con-h 0x00".encode())
         lock.release()
 
-def threaded(client, lock):
+def threaded(connection, lock):
     global maxMessages
     maxMessages = 0
     msgNumber = 0
-    previousMsgNumber = 0
 
     while True:
         if maxMessages <= selectedMaxMessages:
             text = input()
+
             lock.acquire()
-            client.send(("msg-" + str(msgNumber) + "=" + text).encode())
+            connection.send(("msg-" + str(msgNumber) + "=" + text).encode())
             lock.release()
-            previousMsgNumber = msgNumber
-            msgNumber = previousMsgNumber + 2
+
             maxMessages = maxMessages + 1
+            msgNumber = msgNumber + 2
 
-        data = client.recv(4096)
-
+        data = connection.recv(4096)
         from_server = data.decode()
         print(from_server)
 
-        if from_server.startswith("con-res 0xFE"):
-            client.close()
-            print("Timeout occured")
-        if not from_server.startswith("res-" + str(msgNumber - 1)):
-            client.close()
-            print("Protocol ended")
+        if from_server == "con-res 0xFE":
+            connection.close()
+            print("Timeout occured...")
+            break
+
+        if not from_server.startswith("res-") and not ((msgNumber - extractNumber(from_server)) == 1):
+            connection.close()
+            print("Protocol corrupted - connection ended...")
+            break
+
+def extractNumber(from_server):
+    global serverNumber
+    if not from_server.startswith("com-0"):
+        string = from_server.replace('res-', '', 1).replace('=', ' ', 1)
+        numberSearch = [int(i) for i in string.split() if i.isdigit()]
+        serverNumber = numberSearch[0]
+        return int(serverNumber)
 
 def optionsfile():
     file = open("opt.conf")
--- Tech - Server/ServerThread.py
+++ Tech - Server/ServerThread.py
@@ -14,10 +14,10 @@ def timeout():
         print("in timeout " + str(clientTimeout))
 
         if clientTimeout:
-            conn.send(("con-res 0xFE").encode())
+            connection.send(("con-res 0xFE").encode())
             sleep(1.0)
             print("Connection timed out...")
-            conn.close()
+            connection.close()
         else:
             clientTimeout = True
 
@@ -26,38 +26,57 @@ def threaded():
     global from_client
     from_client = ''
     msgNumber = 1
-    previousMsgNumber = 1
+    handshake = False
 
     while True:
-        data = conn.recv(4096)
+        data = connection.recv(4096)
         clientTimeout = True
+
         from_client = data.decode()
         print(from_client)
 
+        #Checks if incoming messages breaks protocol
         if not from_client.startswith("msg-0"):
             clientTimeout = False
-            if not from_client.startswith("msg-" + str(msgNumber - 1)) and not from_client.startswith("com-0") and not from_client.startswith("con-h 0x00"):
-                conn.close()
+            if not from_client.startswith("com-0") and not from_client.startswith("con-h 0x00") and ((msgNumber - extractNumber(from_client)) != 1):
+                connection.close()
                 print("Corrupt/false data received - Socket closed")
                 break
 
-        if from_client.startswith("com-0 accept"):
-            with open("log.txt", "a") as file:
-                file.write(str(datetime.datetime.now()) + " successful handshake performed \n")
-                print(str(datetime.datetime.now()) + " successful handshake performed")
-                file.close()
+        #Performs Handshake
+        if not handshake:
+            if from_client.startswith("com-0 ") and not from_client.startswith("com-0 accept") and int(from_client[7:9]) in range(0, 256):
+                clientTimeout = False
+                connection.send(("com-0 accept " + socket.gethostbyname(socket.gethostname())).encode())
 
-        if from_client.startswith("com-0 ") & from_client.__contains__("."):
-            clientTimeout = False
-            conn.send(("com-0 accept " + socket.gethostbyname(socket.gethostname())).encode())
+            if from_client == "com-0 accept":
+                clientTimeout = False
+                handshake = True
+                writeToLog()
 
-        if from_client.startswith("msg-"):
+        #Checks for correct message number and replies the client
+        if from_client.startswith("msg-") and ((msgNumber - extractNumber(from_client)) == 1):
             clientTimeout = False
-            previousMsgNumber = msgNumber
-            conn.send(("res-" + str(msgNumber) + "=I AM SERVER").encode())
-            msgNumber = previousMsgNumber + 2
+            connection.send(("res-" + str(msgNumber) + "=I AM SERVER").encode())
+            msgNumber = msgNumber+2
 
         from_client = ''
+
+
+def writeToLog():
+    with open("log.txt", "a") as file:
+        print(str(datetime.datetime.now()) + " successful handshake performed")
+        file.write(str(datetime.datetime.now()) + " successful handshake performed \n")
+        file.close()
+
+
+def extractNumber(from_client):
+    global clientNumber
+    if not from_client.startswith("com-0"):
+        string = from_client.replace('msg-', '', 1).replace('=', ' ', 1)
+        numberSearch = [int(i) for i in string.split() if i.isdigit()]
+        clientNumber = numberSearch[0]
+        return int(clientNumber)
 
 def Main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -66,8 +85,8 @@ def Main():
 
     print("Listening...")
     while True:
-        global conn
-        conn, addr = server.accept()
+        global connection
+        connection, addr = server.accept()
         threading.Thread(target=threaded).start()
         threading.Thread(target=timeout).start()
 
